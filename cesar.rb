0b3679afef7764ec700e36232ce0730bde6a5e59
Raouf <Djekboub@gmail.com>
2018-04-10 13:15:26 +0200

    modif du nombre de decalage

--- cesar.rb
+++ cesar.rb
@@ -1,5 +1,5 @@
-def chiffre_de_cesar(input, n)
-	chiffres = input.split("")
+def chiffre_de_cesar(moncode, n)
+	chiffres = moncode.split("")
 	n.times do
 		chiffres.each do |i| 
 			if (i.ord >= 97 && i.ord <= 122) || (i.ord >= 65 && i.ord <= 90)
@@ -16,5 +16,6 @@ def chiffre_de_cesar(input, n)
  	chiffres.join("")
 end
 
-puts chiffre_de_cesar("what a string", 3) # salut correcteur, si tu veux tester notre code pour voir s'il fonctionne.
+puts chiffre_de_cesar("what a string", 5) # salut correcteur, si tu veux tester notre code pour voir s'il fonctionne.
 # t'as juste a taper la phrase que tu veux a la place de "what a string" allé salut champion !
+#tu verras sur la console que "bmfy f xywnsl" va s'afficher comme demandé dans l'exo.
